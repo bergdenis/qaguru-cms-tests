956504aa564fa2bbb15dd5f796da3f8e288ba65b
Denis Berg <bergdenis@seznam.cz>
2026-06-04 00:15:40 +0200

    Update lesson 6 homework: improve readable_name function

--- homework.py
+++ homework.py
@@ -89,16 +89,23 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def print_function_name_and_args(func, *args):
+    func_name = func.__name__.replace('_', ' ').title()
+    args_name = ", ".join([*args])
+    f"{func_name} [{args_name}]"
+    return f"{func_name} [{args_name}]"
+
+
 def open_browser(browser_name):
-    actual_result = inspect.currentframe().f_code.co_name.replace("_", " ").title() + f" [{browser_name}]"
+    actual_result = print_function_name_and_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = inspect.currentframe().f_code.co_name.replace("_", " ").title() + f" [{page_url}]"
+    actual_result = print_function_name_and_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = inspect.currentframe().f_code.co_name.replace("_", " ").title() + f" [{page_url}, {button_text}]"
+    actual_result = print_function_name_and_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
